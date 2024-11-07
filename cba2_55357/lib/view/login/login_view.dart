6f6ec6618e3ea999da6923b9f67571342a39f61b
Twoje Imię Nazwisko <twoj@email.com>
2024-11-07 23:22:07 +0100

    textFiled added

--- cba2_55357/lib/view/login/login_view.dart
+++ cba2_55357/lib/view/login/login_view.dart
@@ -15,11 +15,59 @@ class LoginView extends StatelessWidget {
           width: double.infinity,
           child: Column(
             children: [
+              SizedBox(height: 62), // Przestrzeń od góry
+
               Image.asset(MyImages.logo),
-              Text('Sign In', style: TextStyle(fontSize: 30, fontWeight: FontWeight.w700,
-              color: MyColors.purleColor,
-              )
+              SizedBox(height: 20), // Odstęp między grafiką a tekstem
+
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+                padding: const EdgeInsets.only(left: 19.0),
+                child: Text('Sign In', style: TextStyle(fontSize: 30, fontWeight: FontWeight.w700,
+                color: MyColors.purleColor,
+                )
+                ),
               ),
+          ),
+              SizedBox(height: 20), // Odstęp między tekstem a polem tekstowym
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal:21.0),
+                child: Container(
+                  height: 50,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'Email or User name', // Podpowiedź dla pierwszego pola
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.person),
+                    ),
+                    keyboardType: TextInputType.emailAddress, // Klawiatura dostosowana do e-maili
+                  ),
+                ),
+              ),
+          ),
+              SizedBox(height: 16), // Odstęp między pierwszym a drugim polem
+          // Drugie pole
+          Align(
+            alignment: Alignment.centerLeft,
+          child:  Padding(
+            padding: const EdgeInsets.symmetric(horizontal:21.0),
+            child: Container(
+              width: 390,
+              height: 50,
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: 'Password', // Podpowiedź
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.lock),
+                ),
+                obscureText: true, // Ukrycie tekstu (hasło)
+              ),
+            ),
+          ),
+          ),
               ElevatedButton(
                 child: const Text('Zaloguj się'),
                 onPressed: () {
