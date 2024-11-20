06884a5cbc8ffc78b3c03303cdf4dd2b339ea639
Twoje Imię Nazwisko <twoj@email.com>
2024-11-20 23:20:08 +0100

    Forget Password tekst added

--- cba2_55357/lib/view/login/login_view.dart
+++ cba2_55357/lib/view/login/login_view.dart
@@ -31,6 +31,7 @@ class LoginView extends StatelessWidget {
                       )),
                 ),
               ),
+
               SizedBox(height: 20), // Odstęp między tekstem a polem tekstowym
               Align(
                 alignment: Alignment.centerLeft,
@@ -54,48 +55,65 @@ class LoginView extends StatelessWidget {
               SizedBox(height: 16), // Odstęp między pierwszym a drugim polem
               // Drugie pole
               Align(
-                alignment: Alignment.centerLeft,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 21.0),
-                  child: Container(
-                    width: 390,
-                    height: 50,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        hintText: 'Password', // Podpowiedź
-                        border: OutlineInputBorder(),
-                        prefixIcon: Icon(Icons.lock),
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 21.0),
+                    child: Container(
+                      //width: 390,
+                      height: 50,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Password', // Podpowiedź
+                          border: OutlineInputBorder(),
+                          prefixIcon: Icon(Icons.lock),
+                        ),
+                        obscureText: true, // Ukrycie tekstu (hasło)
                       ),
-                      obscureText: true, // Ukrycie tekstu (hasło)
                     ),
                   ),
+                  ),
+
+              Align(
+                alignment: Alignment.centerRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 21.0, top: 16),                  
+                  child: Text('Forget Password ?',
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.w700,
+                        color: MyColors.purleColor,
+                      )),
                 ),
               ),
-          Center(
-            child: Padding(
-                padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.purple, // kolor tła przycisku
-                    minimumSize: const Size(390, 50),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15), // zaokrąglenie na 15px
-                    ),// szerokość i wysokość
+
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.purple, // kolor tła przycisku
+                      minimumSize: const Size(390, 50),
+                      shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(15), // zaokrąglenie na 15px
+                      ), // szerokość i wysokość
+                    ),
+                    child: const Text(
+                      'Zaloguj się',
+                      style: TextStyle(
+                          color: Colors.white), // kolor tekstu na biały),
+                    ),
+                    onPressed: () {
+                      // Navigate to second route when tapped.
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const RegisterView()),
+                      );
+                    },
                   ),
-                  child: const Text('Zaloguj się',
-                    style: TextStyle(color: Colors.white), // kolor tekstu na biały),
-                  ),
-                  onPressed: () {
-                    // Navigate to second route when tapped.
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const RegisterView()),
-                    );
-                  },
                 ),
               ),
-          ),
             ],
           ),
         ),
